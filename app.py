27e6399347155a7ab12fcb5dacb5ddaf0aff6f05
Fitryrositadesviani <fitryrosita123@gmail.com>
2025-06-20 00:14:25 +0700

    Update app.py

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ soal_pilgan = [
 # ===================== TAMPILAN UTAMA =====================
 st.set_page_config(page_title="Kuis Interaktif Kegiatan 2", page_icon="🎮")
 st.title("🎮 Kuis Interaktif - Kegiatan 2")
-st.image("https://th.bing.com/th/id/OIP.xEY-5vSB5erwdxAJeEFAjwHaFj?r=0&rs=1&pid=ImgDetMain", use_container_width=True)
+st.image("https://pin.it/4Fn6ObRr7", use_container_width=True)
 st.caption("Mengangkat nilai budaya dalam pembelajaran peluang 🎲 melalui permainan tradisional Cublak-Cublak Suweng.")
 
 # ===================== INPUT NAMA SISWA =====================
